ddac643c8de741fc6e9395ba8a6a7a3fbadb77a7
maxlonergan <111542631+maxlonergan@users.noreply.github.com>
2022-11-24 10:06:29 -0700

    Update dataplot.py

--- dataplot.py
+++ dataplot.py
@@ -1,29 +1,35 @@
 '''
 data visualization project
 '''
-
+import math
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
-import glob 
-
-data_one = np.loadtxt('2_Record2308.dat')
-data_two = np.loadtxt('2_Record3388.dat')
-data_three = np.loadtxt('as_ch01-0537xx_Record1042.dat')
-
-xpoints = np.array([0,6])
-ypoints = np.array([0,250])
 
 
-plt.plot(data_one)
+def smoothed_array(arr):
+    smoothed_data = np.array([])
+    for i in range(3,len(arr)-4):
+        smooth_point = math.floor((arr[i-3]+arr[i-2]+arr[i-1]+arr[i]+arr[i+1]+arr[i+2]+arr[i+3])/15)
+        smoothed_data = np.append(smoothed_data, smooth_point)
+    return smoothed_data
+
+def analyze(file):
+    file_name = file
+    print(file_name)
+    data = np.loadtxt(file)
+    smoothed_data = smoothed_array(data)
+    plt.plot(data)
+    plt.plot(smoothed_data)
+    # plt.savefig()
+
+def main():
+    for fname in glob.glob('*.dat'):
+        analyze(fname)
+
+main()
+
+# plt.plot(data_one)
 # plt.plot(data_two)
-# # plt.savefig('2_Record2308.pdf')
-plt.show()
+# plt.show()
 
-# def main():
-#     for fname in glob.glob('*.dat'):
-#         analyze(fname)
-
-# def analyze(file):
-#     data = np.loadtxt(file)
-#     plt.plot(data)
-#     plt.show()
