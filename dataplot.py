4a623e040708baa04e54e7da21302a7911ad66a3
maxlonergan <111542631+maxlonergan@users.noreply.github.com>
2022-11-24 11:01:14 -0700

    Update dataplot.py
    working on detecting pulses

--- dataplot.py
+++ dataplot.py
@@ -15,32 +15,26 @@ def smoothed_array(arr):
     return smoothed_data
 
 def plot(old, new, pdf):
-    plt.subplot(2,1,1)
-    plt.plot(old)
-    plt.title('Rough')
-    plt.subplot(2,1,2)
-    
-    plt.plot(new)
-    plt.xlabel('Smooth')
+    _,axes = plt.subplots(nrows=2)
+
+    axes[0].plot(old)
+    axes[0].set(title='Rough')
+
+    axes[1].set(ylabel='Smooth')
+    axes[1].plot(new)
     plt.savefig(pdf)
 
 def analyze(file):
     file_name = file.replace('dat', 'pdf')
     data = np.loadtxt(file)
     smoothed_data = smoothed_array(data)
-    plot(data, smoothed_data, file_name)
-    # plt.subplot(2,1,1)
-    # plt.plot(data)
-    # plt.title('Rough')
-    # plt.subplot(2,1,2)
-    # plt.plot(smoothed_data)
-    # plt.xlabel('Smooth')
-    # plt.savefig(file_name)
-    
+    for num in smoothed_data:
+        pos = smoothed_data[num]
+        pass
+    # plot(data, smoothed_data, file_name)
 
 def main():
     for fname in glob.glob('*.dat'):
         analyze(fname)
 
 main()
-
