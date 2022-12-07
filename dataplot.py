d15f7431623ccde4fd7faedc9820b21c7853e68a
maxlonergan <111542631+maxlonergan@users.noreply.github.com>
2022-12-07 12:44:54 -0700

    update dataplot.py

--- dataplot.py
+++ dataplot.py
@@ -5,12 +5,12 @@ import math
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
-
+WIDTH = 50
 
 def smoothed_array(arr):
     smoothed_data = np.array([])
-    for i in range(3,len(arr)-4):
-        smooth_point = math.floor((arr[i-3]+arr[i-2]+arr[i-1]+arr[i]+arr[i+1]+arr[i+2]+arr[i+3])/15)
+    for i in range(3,len(arr)-3):
+        smooth_point = math.floor((arr[i-3] + 2 * arr[i-2] + 3 * arr[i-1] + 3 * arr[i] + 3 * arr[i+1] + 2 * arr[i+2] + arr[i+3])/15)
         smoothed_data = np.append(smoothed_data, smooth_point)
     return smoothed_data
 
@@ -29,13 +29,28 @@ def analyze(file):
     raw_data = np.loadtxt(file)
     smoothed_data = smoothed_array(raw_data)
     pulses = []
-    for i in range(len(raw_data)-2):
-        y = raw_data[i]
-        y_plus_two = raw_data[i+2]
-        vt = 100
-        if y_plus_two - y > vt:
+    voltage_threshold = 100
+
+    for i in range(len(smoothed_data)-2):
+        y = abs(smoothed_data[i])
+        y_plus_two = abs(smoothed_data[i+2])
+        if y_plus_two - y > voltage_threshold:
             pulse_start = i
             pulses.append(pulse_start)
+
+    # for i in range(len(pulses)):
+    #     start_position = pulses[i]
+    #     real_width = WIDTH
+
+    #     if i < len(pulses) - 1 and pulses[i] + real_width > pulses[i + 1]:
+    #         real_width = pulses[i +1] - start_position
+    #     real_width = min(real_width, len(smoothed_data) - start_position)
+    #     area = int(sum(raw_data[start_position:start_position + real_width]))
+    #     output_string = f'Pulse {i+1}: {start_position+1} ({area})\n'
+    #     print(output_string)
+
+    for i in range(1016,1021):
+        print(smoothed_data[i])
     print(pulses)
     plot(raw_data, smoothed_data, file_name)
 
